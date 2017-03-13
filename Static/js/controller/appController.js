86ad00d822d4ec8a9b3df0ab67215a948d02a17a
Nimra Wilayat <Nimra Wilayat>
2017-03-13 18:44:51 +0500

    First Commit 13/March/2017

--- Static/js/controller/appController.js
+++ Static/js/controller/appController.js
@@ -10,7 +10,7 @@
            // Configure a dark theme with primary foreground yellow
 
            $mdThemingProvider.theme('docs-dark', 'default')
-               .primaryPalette('green')
+               .primaryPalette('yellow')
                .dark();
        });
 })();
